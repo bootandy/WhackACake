078d9fa5ca9b6b6e21a0a5fedf351fcf835b4f7a
Richard Green <richard@barcodebeasties.com>
2011-11-12 21:17:55 +0000

    initial integratin

--- android/WhackACake/src/com/oxygenaccelerator/pudsey/GameActivity.java
+++ android/WhackACake/src/com/oxygenaccelerator/pudsey/GameActivity.java
@@ -1,43 +1,63 @@
 package com.oxygenaccelerator.pudsey;
 
 import java.io.File;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 
 public class GameActivity  extends Activity{
+	
+	private static final String LOG_TAG = "GameActivity";
 	
 	 @Override
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        
 	        setContentView(R.layout.game);
+	            
 	        
-	        WebView webview = (WebView) findViewById(R.id.webview);
+	        final WebView webview = (WebView) findViewById(R.id.webview);
+	        
+	        WebSettings ws = webview.getSettings();
+	        
+	        ws.setJavaScriptEnabled(true);
+	        
+	        webview.setWebChromeClient(new MyWebChromeClient());
+	        
+	        /* WebViewClient must be set BEFORE calling loadUrl! */  
+	        webview.setWebViewClient(new WebViewClient() {  
+	            @Override  
+	            public void onPageFinished(WebView view, String url)  
+	            {  
+	                webview.loadUrl("javascript:(function() { " +  
+	                        "whackacake.init(); " +  
+	                        "})()");  
+	            }  
+	        });  
 	        
 	        
-	        webview.loadUrl("file:///android_asset/test.html");
+	        webview.loadUrl("file:///android_asset/index.html");
 	        
-	        
-	        /*
-	        WebView webview = (WebView) findViewById(R.id.webview);
-	        
-	        File f = new File("file:///android_asset/test.html");
-	        
-	        String exists = "does not exist";
-	        
-	        if (f.exists()) {
-	        	exists = "exists";
-	        }
-	        
-	        Log.d("GameActivity",exists);
-	        
-	       // webview.loadUrl("file:///android_asset/test.html");
-	        */
-	        
+	               
 	 }
+	 
+	 final class MyWebChromeClient extends WebChromeClient {
+	        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+	            Log.d(LOG_TAG, message);
+	            result.confirm();
+	            return true;
+	        }
+	    }
+	 
+	 
+	 
 
 }
