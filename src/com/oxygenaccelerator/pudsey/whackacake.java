33cbae82b6c040e889ae4d4722f56446f60e3007
Richard Green <richard@barcodebeasties.com>
2011-11-12 12:02:47 +0000

    initial checkin

---
+++ src/com/oxygenaccelerator/pudsey/whackacake.java
@@ -0,0 +1,13 @@
+package com.oxygenaccelerator.pudsey;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class whackacake extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
