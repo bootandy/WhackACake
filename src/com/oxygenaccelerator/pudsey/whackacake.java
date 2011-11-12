3a83a4c9438ea0376b7b392700bc38ef2ad2e6b6
Richard Green <richard@barcodebeasties.com>
2011-11-12 12:31:48 +0000

    got something running now

--- src/com/oxygenaccelerator/pudsey/whackacake.java
+++ src/com/oxygenaccelerator/pudsey/whackacake.java
@@ -1,7 +1,10 @@
 package com.oxygenaccelerator.pudsey;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class whackacake extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,34 @@ public class whackacake extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        final Button donate_button = (Button) findViewById(R.id.button_donate);
+        donate_button.setOnClickListener(new View.OnClickListener() {
+        	
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent("com.oxygenaccelerator.pudsey.DONATE");
+				startActivity(i);
+	        }
+				
+			});
+        
+        
+        
+        final Button game_button = (Button) findViewById(R.id.button_playgame);
+        game_button.setOnClickListener(new View.OnClickListener() {
+        	
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent("com.oxygenaccelerator.pudsey.GAME");
+				startActivity(i);
+	        }
+				
+			});
+        
+        
     }
 }
